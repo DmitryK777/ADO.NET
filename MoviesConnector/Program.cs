39e78f0193da37cd4bd5f1a45e6093ef917fb2a1
DmitryK777 <kislyakovdmv@gmail.com>
2025-03-23 16:26:46 +0300

    Class work

--- MoviesConnector/Program.cs
+++ MoviesConnector/Program.cs
@@ -10,6 +10,7 @@ namespace MoviesConnector
 	{
 		static void Main(string[] args)
 		{
+			/*
 			const string CONNECTION_STRING =
 				"Data Source = (localdb)\\MSSQLLocalDB;" +
 				"Initial Catalog = Movies_VPD_311;" +
@@ -18,11 +19,25 @@ namespace MoviesConnector
 				"TrustServerCertificate = False;" +
 				"ApplicationIntent = ReadWrite;" +
 				"MultiSubnetFailover = False";
+			*/
+			//Connector connector = new Connector(CONNECTION_STRING);
 
-			Connector connector = new Connector(CONNECTION_STRING);
+			Connector connector = new Connector();
 
-			connector.Select("SELECT * FROM Directors");
-			connector.Select("SELECT * FROM Movies");
+			//connector.Select("SELECT * FROM Directors");
+			//connector.Select("SELECT * FROM Movies");
+
+			
+
+			connector.InsertDirector("Martin", "Scorsese");
+			connector.Select("*", "Directors");
+
+			connector.Select(
+				"title, release_date, FORMATMESSAGE(N'%s %s', first_name, last_name) AS N'Режиссёр'", 
+				"Movies, Directors", 
+				"director = director_id"
+				);
+
 		}
 	}
 }
