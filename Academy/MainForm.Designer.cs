074606951519f74180af2b3f873bf9eaf75e227a
DmitryK777 <kislyakovdmv@gmail.com>
2025-04-02 22:34:34 +0300

    HomeWork is DONE

--- Academy/MainForm.Designer.cs
+++ Academy/MainForm.Designer.cs
@@ -30,29 +30,34 @@
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.statusStrip = new System.Windows.Forms.StatusStrip();
+			this.statusStripCountLabel = new System.Windows.Forms.ToolStripStatusLabel();
 			this.tabControl = new System.Windows.Forms.TabControl();
 			this.tabPageStudents = new System.Windows.Forms.TabPage();
-			this.tabPageGroups = new System.Windows.Forms.TabPage();
-			this.tabPageDirections = new System.Windows.Forms.TabPage();
-			this.tabPageDisciplines = new System.Windows.Forms.TabPage();
-			this.tabPageTeachers = new System.Windows.Forms.TabPage();
+			this.comboBox_Students_Groups = new System.Windows.Forms.ComboBox();
+			this.comboBox_Students_Directions = new System.Windows.Forms.ComboBox();
 			this.dgvStudents = new System.Windows.Forms.DataGridView();
+			this.tabPageGroups = new System.Windows.Forms.TabPage();
+			this.comboBox_Groups_Directions = new System.Windows.Forms.ComboBox();
 			this.dgvGroups = new System.Windows.Forms.DataGridView();
+			this.tabPageDirections = new System.Windows.Forms.TabPage();
 			this.dgvDirections = new System.Windows.Forms.DataGridView();
+			this.tabPageDisciplines = new System.Windows.Forms.TabPage();
 			this.dgvDisciplines = new System.Windows.Forms.DataGridView();
+			this.tabPageTeachers = new System.Windows.Forms.TabPage();
 			this.dgvTeachers = new System.Windows.Forms.DataGridView();
-			this.statusStripCountLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.checkBox_Empty_Groups = new System.Windows.Forms.CheckBox();
+			this.checkBox_Empty_Directions = new System.Windows.Forms.CheckBox();
 			this.statusStrip.SuspendLayout();
 			this.tabControl.SuspendLayout();
 			this.tabPageStudents.SuspendLayout();
-			this.tabPageGroups.SuspendLayout();
-			this.tabPageDirections.SuspendLayout();
-			this.tabPageDisciplines.SuspendLayout();
-			this.tabPageTeachers.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+			this.tabPageGroups.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dgvGroups)).BeginInit();
+			this.tabPageDirections.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dgvDirections)).BeginInit();
+			this.tabPageDisciplines.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dgvDisciplines)).BeginInit();
+			this.tabPageTeachers.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dgvTeachers)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -65,6 +70,12 @@
 			this.statusStrip.Size = new System.Drawing.Size(800, 22);
 			this.statusStrip.TabIndex = 0;
 			this.statusStrip.Text = "statusStrip1";
+			// 
+			// statusStripCountLabel
+			// 
+			this.statusStripCountLabel.Name = "statusStripCountLabel";
+			this.statusStripCountLabel.Size = new System.Drawing.Size(123, 17);
+			this.statusStripCountLabel.Text = "statusStripCountLabel";
 			// 
 			// tabControl
 			// 
@@ -84,6 +95,8 @@
 			// tabPageStudents
 			// 
 			this.tabPageStudents.BackColor = System.Drawing.Color.Azure;
+			this.tabPageStudents.Controls.Add(this.comboBox_Students_Groups);
+			this.tabPageStudents.Controls.Add(this.comboBox_Students_Directions);
 			this.tabPageStudents.Controls.Add(this.dgvStudents);
 			this.tabPageStudents.Location = new System.Drawing.Point(4, 22);
 			this.tabPageStudents.Name = "tabPageStudents";
@@ -92,49 +105,27 @@
 			this.tabPageStudents.TabIndex = 0;
 			this.tabPageStudents.Text = "Students";
 			// 
-			// tabPageGroups
+			// comboBox_Students_Groups
 			// 
-			this.tabPageGroups.BackColor = System.Drawing.Color.Azure;
-			this.tabPageGroups.Controls.Add(this.dgvGroups);
-			this.tabPageGroups.Location = new System.Drawing.Point(4, 22);
-			this.tabPageGroups.Name = "tabPageGroups";
-			this.tabPageGroups.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPageGroups.Size = new System.Drawing.Size(792, 402);
-			this.tabPageGroups.TabIndex = 1;
-			this.tabPageGroups.Text = "Groups";
+			this.comboBox_Students_Groups.FormattingEnabled = true;
+			this.comboBox_Students_Groups.Location = new System.Drawing.Point(224, 7);
+			this.comboBox_Students_Groups.Name = "comboBox_Students_Groups";
+			this.comboBox_Students_Groups.Size = new System.Drawing.Size(202, 21);
+			this.comboBox_Students_Groups.TabIndex = 2;
+			this.comboBox_Students_Groups.Text = "Groups";
+			this.comboBox_Students_Groups.SelectedIndexChanged += new System.EventHandler(this.comboBox_Students_Groups_SelectedIndexChanged);
+			this.comboBox_Students_Groups.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox_Students_Groups_MouseClick);
 			// 
-			// tabPageDirections
+			// comboBox_Students_Directions
 			// 
-			this.tabPageDirections.BackColor = System.Drawing.Color.Azure;
-			this.tabPageDirections.Controls.Add(this.dgvDirections);
-			this.tabPageDirections.Location = new System.Drawing.Point(4, 22);
-			this.tabPageDirections.Name = "tabPageDirections";
-			this.tabPageDirections.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPageDirections.Size = new System.Drawing.Size(792, 402);
-			this.tabPageDirections.TabIndex = 2;
-			this.tabPageDirections.Text = "Directions";
-			// 
-			// tabPageDisciplines
-			// 
-			this.tabPageDisciplines.BackColor = System.Drawing.Color.Azure;
-			this.tabPageDisciplines.Controls.Add(this.dgvDisciplines);
-			this.tabPageDisciplines.Location = new System.Drawing.Point(4, 22);
-			this.tabPageDisciplines.Name = "tabPageDisciplines";
-			this.tabPageDisciplines.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPageDisciplines.Size = new System.Drawing.Size(792, 402);
-			this.tabPageDisciplines.TabIndex = 3;
-			this.tabPageDisciplines.Text = "Disciplines";
-			// 
-			// tabPageTeachers
-			// 
-			this.tabPageTeachers.BackColor = System.Drawing.Color.Azure;
-			this.tabPageTeachers.Controls.Add(this.dgvTeachers);
-			this.tabPageTeachers.Location = new System.Drawing.Point(4, 22);
-			this.tabPageTeachers.Name = "tabPageTeachers";
-			this.tabPageTeachers.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPageTeachers.Size = new System.Drawing.Size(792, 402);
-			this.tabPageTeachers.TabIndex = 4;
-			this.tabPageTeachers.Text = "Teachers";
+			this.comboBox_Students_Directions.FormattingEnabled = true;
+			this.comboBox_Students_Directions.Location = new System.Drawing.Point(9, 7);
+			this.comboBox_Students_Directions.Name = "comboBox_Students_Directions";
+			this.comboBox_Students_Directions.Size = new System.Drawing.Size(208, 21);
+			this.comboBox_Students_Directions.TabIndex = 1;
+			this.comboBox_Students_Directions.Text = "Directions";
+			this.comboBox_Students_Directions.SelectedIndexChanged += new System.EventHandler(this.comboBox_Students_Directions_SelectedIndexChanged);
+			this.comboBox_Students_Directions.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox_Students_Directions_MouseClick);
 			// 
 			// dgvStudents
 			// 
@@ -148,6 +139,31 @@
 			this.dgvStudents.Size = new System.Drawing.Size(780, 355);
 			this.dgvStudents.TabIndex = 0;
 			// 
+			// tabPageGroups
+			// 
+			this.tabPageGroups.BackColor = System.Drawing.Color.Azure;
+			this.tabPageGroups.Controls.Add(this.checkBox_Empty_Groups);
+			this.tabPageGroups.Controls.Add(this.comboBox_Groups_Directions);
+			this.tabPageGroups.Controls.Add(this.dgvGroups);
+			this.tabPageGroups.Location = new System.Drawing.Point(4, 22);
+			this.tabPageGroups.Name = "tabPageGroups";
+			this.tabPageGroups.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageGroups.Size = new System.Drawing.Size(792, 402);
+			this.tabPageGroups.TabIndex = 1;
+			this.tabPageGroups.Text = "Groups";
+			// 
+			// comboBox_Groups_Directions
+			// 
+			this.comboBox_Groups_Directions.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.comboBox_Groups_Directions.FormattingEnabled = true;
+			this.comboBox_Groups_Directions.Location = new System.Drawing.Point(9, 7);
+			this.comboBox_Groups_Directions.Name = "comboBox_Groups_Directions";
+			this.comboBox_Groups_Directions.Size = new System.Drawing.Size(214, 21);
+			this.comboBox_Groups_Directions.TabIndex = 1;
+			this.comboBox_Groups_Directions.Text = "Directions";
+			this.comboBox_Groups_Directions.SelectedIndexChanged += new System.EventHandler(this.comboBox_Groups_Directions_SelectedIndexChanged);
+			this.comboBox_Groups_Directions.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox_Groups_Directions_MouseClick);
+			// 
 			// dgvGroups
 			// 
 			this.dgvGroups.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -159,6 +175,18 @@
 			this.dgvGroups.Name = "dgvGroups";
 			this.dgvGroups.Size = new System.Drawing.Size(780, 356);
 			this.dgvGroups.TabIndex = 0;
+			// 
+			// tabPageDirections
+			// 
+			this.tabPageDirections.BackColor = System.Drawing.Color.Azure;
+			this.tabPageDirections.Controls.Add(this.checkBox_Empty_Directions);
+			this.tabPageDirections.Controls.Add(this.dgvDirections);
+			this.tabPageDirections.Location = new System.Drawing.Point(4, 22);
+			this.tabPageDirections.Name = "tabPageDirections";
+			this.tabPageDirections.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageDirections.Size = new System.Drawing.Size(792, 402);
+			this.tabPageDirections.TabIndex = 2;
+			this.tabPageDirections.Text = "Directions";
 			// 
 			// dgvDirections
 			// 
@@ -172,6 +200,17 @@
 			this.dgvDirections.Size = new System.Drawing.Size(780, 358);
 			this.dgvDirections.TabIndex = 0;
 			// 
+			// tabPageDisciplines
+			// 
+			this.tabPageDisciplines.BackColor = System.Drawing.Color.Azure;
+			this.tabPageDisciplines.Controls.Add(this.dgvDisciplines);
+			this.tabPageDisciplines.Location = new System.Drawing.Point(4, 22);
+			this.tabPageDisciplines.Name = "tabPageDisciplines";
+			this.tabPageDisciplines.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageDisciplines.Size = new System.Drawing.Size(792, 402);
+			this.tabPageDisciplines.TabIndex = 3;
+			this.tabPageDisciplines.Text = "Disciplines";
+			// 
 			// dgvDisciplines
 			// 
 			this.dgvDisciplines.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -183,6 +222,17 @@
 			this.dgvDisciplines.Name = "dgvDisciplines";
 			this.dgvDisciplines.Size = new System.Drawing.Size(780, 357);
 			this.dgvDisciplines.TabIndex = 0;
+			// 
+			// tabPageTeachers
+			// 
+			this.tabPageTeachers.BackColor = System.Drawing.Color.Azure;
+			this.tabPageTeachers.Controls.Add(this.dgvTeachers);
+			this.tabPageTeachers.Location = new System.Drawing.Point(4, 22);
+			this.tabPageTeachers.Name = "tabPageTeachers";
+			this.tabPageTeachers.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageTeachers.Size = new System.Drawing.Size(792, 402);
+			this.tabPageTeachers.TabIndex = 4;
+			this.tabPageTeachers.Text = "Teachers";
 			// 
 			// dgvTeachers
 			// 
@@ -196,11 +246,29 @@
 			this.dgvTeachers.Size = new System.Drawing.Size(780, 360);
 			this.dgvTeachers.TabIndex = 0;
 			// 
-			// statusStripCountLabel
+			// checkBox_Empty_Groups
 			// 
-			this.statusStripCountLabel.Name = "statusStripCountLabel";
-			this.statusStripCountLabel.Size = new System.Drawing.Size(123, 17);
-			this.statusStripCountLabel.Text = "statusStripCountLabel";
+			this.checkBox_Empty_Groups.AutoSize = true;
+			this.checkBox_Empty_Groups.Font = new System.Drawing.Font("Arial Narrow", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.checkBox_Empty_Groups.Location = new System.Drawing.Point(295, 7);
+			this.checkBox_Empty_Groups.Name = "checkBox_Empty_Groups";
+			this.checkBox_Empty_Groups.Size = new System.Drawing.Size(202, 24);
+			this.checkBox_Empty_Groups.TabIndex = 3;
+			this.checkBox_Empty_Groups.Text = "Показать пустые группы";
+			this.checkBox_Empty_Groups.UseVisualStyleBackColor = true;
+			this.checkBox_Empty_Groups.CheckedChanged += new System.EventHandler(this.checkBox_Empty_Groups_CheckedChanged);
+			// 
+			// checkBox_Empty_Directions
+			// 
+			this.checkBox_Empty_Directions.AutoSize = true;
+			this.checkBox_Empty_Directions.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.checkBox_Empty_Directions.Location = new System.Drawing.Point(250, 7);
+			this.checkBox_Empty_Directions.Name = "checkBox_Empty_Directions";
+			this.checkBox_Empty_Directions.Size = new System.Drawing.Size(283, 22);
+			this.checkBox_Empty_Directions.TabIndex = 1;
+			this.checkBox_Empty_Directions.Text = "Показать пустые направления";
+			this.checkBox_Empty_Directions.UseVisualStyleBackColor = true;
+			this.checkBox_Empty_Directions.CheckedChanged += new System.EventHandler(this.checkBox_Empty_Directions_CheckedChanged);
 			// 
 			// MainForm
 			// 
@@ -211,19 +279,22 @@
 			this.Controls.Add(this.statusStrip);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
 			this.Text = "Academy VPD_311";
 			this.statusStrip.ResumeLayout(false);
 			this.statusStrip.PerformLayout();
 			this.tabControl.ResumeLayout(false);
 			this.tabPageStudents.ResumeLayout(false);
-			this.tabPageGroups.ResumeLayout(false);
-			this.tabPageDirections.ResumeLayout(false);
-			this.tabPageDisciplines.ResumeLayout(false);
-			this.tabPageTeachers.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+			this.tabPageGroups.ResumeLayout(false);
+			this.tabPageGroups.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dgvGroups)).EndInit();
+			this.tabPageDirections.ResumeLayout(false);
+			this.tabPageDirections.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dgvDirections)).EndInit();
+			this.tabPageDisciplines.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.dgvDisciplines)).EndInit();
+			this.tabPageTeachers.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.dgvTeachers)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
@@ -245,6 +316,11 @@
 		private System.Windows.Forms.DataGridView dgvDisciplines;
 		private System.Windows.Forms.DataGridView dgvTeachers;
 		private System.Windows.Forms.ToolStripStatusLabel statusStripCountLabel;
+		private System.Windows.Forms.ComboBox comboBox_Groups_Directions;
+		private System.Windows.Forms.ComboBox comboBox_Students_Groups;
+		private System.Windows.Forms.ComboBox comboBox_Students_Directions;
+		private System.Windows.Forms.CheckBox checkBox_Empty_Groups;
+		private System.Windows.Forms.CheckBox checkBox_Empty_Directions;
 	}
 }
 
--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -18,19 +18,31 @@ namespace Academy
 
 		Query[] queries = new Query[]
 			{
-				new Query("*", "Students"),
+				new Query
+					(
+						"DISTINCT stud_id, last_name, first_name, middle_name, birth_date, direction_name, group_name", 
+						"Students, Groups, Directions",
+						"group_id = [group] AND direction = direction_id",
+						"stud_id, first_name, last_name, middle_name, birth_date, direction_name, group_name"
+					),
 				new Query
 					(
 						"group_id, group_name, COUNT(stud_id) AS students_count, direction_name", 
 						"Students, Groups, Directions", 
 						"direction = direction_id AND [group] = group_id",
 						"group_id, group_name, direction_name"
-						
 					),
-				new Query("*", "Directions"),
+				new Query
+					(
+						"direction_id, direction_name, COUNT(DISTINCT group_id) AS groups_count, COUNT(stud_id) AS students_count", 
+						"Directions, Groups, Students",
+						"direction_id = direction AND group_id = [group]",
+						"direction_id, direction_name"
+					),
 				new Query("*", "Disciplines"),
 				new Query("*", "Teachers")
 			};
+		DataTable table;
 		DataGridView[] tables;
 		string[] status_messages = new string[]
 			{
@@ -44,6 +56,7 @@ namespace Academy
 		public MainForm()
 		{
 			InitializeComponent();
+			this.Location = new Point((Screen.PrimaryScreen.Bounds.Width/2) - (this.Width/2), 100);
 
 			tables = new DataGridView[]
 			{
@@ -66,5 +79,166 @@ namespace Academy
 			tables[i].DataSource = connector.Select(queries[i].Columns, queries[i].Tables, queries[i].Condition, queries[i].GroupBy);
 			statusStripCountLabel.Text = $"{status_messages[i]} {tables[i].RowCount - 1}";
 		}
+
+		private void comboBox_Groups_Directions_MouseClick(object sender, MouseEventArgs e)
+		{
+			comboBox_Groups_Directions.Items.Clear();
+			Choice_Directions_In_Groups();
+		}
+
+		private void comboBox_Groups_Directions_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string choiced_direction = comboBox_Groups_Directions.Text;
+
+			int i = tabControl.SelectedIndex;
+			if (comboBox_Groups_Directions.SelectedIndex != 0)
+			{
+				comboBox_Groups_Directions.Items.Clear();
+				dgvGroups.DataSource = connector.Select(queries[i].Columns, queries[i].Tables, $"{queries[i].Condition} AND direction_name = '{choiced_direction}'", queries[i].GroupBy);
+				statusStripCountLabel.Text = $"{status_messages[i]} {tables[i].RowCount - 1}";
+			}
+			else
+			{
+				comboBox_Groups_Directions.Items.Clear();
+				Choice_Directions_In_Groups();
+				tables[i].DataSource = connector.Select(queries[i].Columns, queries[i].Tables, queries[i].Condition, queries[i].GroupBy);
+			}
+			statusStripCountLabel.Text = $"{status_messages[i]} {tables[i].RowCount - 1}";
+		}
+
+		
+		private void comboBox_Students_Directions_MouseClick(object sender, MouseEventArgs e)
+		{
+			comboBox_Students_Directions.Items.Clear();
+			Choice_Directions_In_Students();
+		}
+
+		private void comboBox_Students_Directions_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string choiced_direction = comboBox_Students_Directions.Text;
+
+			int i = tabControl.SelectedIndex;
+			if (comboBox_Students_Directions.SelectedIndex != 0)
+			{
+				comboBox_Students_Directions.Items.Clear();
+				dgvStudents.DataSource = connector.Select(queries[i].Columns, queries[i].Tables, $"{queries[i].Condition} AND direction_name = '{choiced_direction}'", queries[i].GroupBy);
+				statusStripCountLabel.Text = $"{status_messages[i]} {tables[i].RowCount - 1}";
+			}
+			else
+			{
+				comboBox_Students_Directions.Items.Clear();
+				Choice_Directions_In_Students();
+				tables[i].DataSource = connector.Select(queries[i].Columns, queries[i].Tables, queries[i].Condition, queries[i].GroupBy);
+			}
+			statusStripCountLabel.Text = $"{status_messages[i]} {tables[i].RowCount - 1}";
+		}
+
+		private void comboBox_Students_Groups_MouseClick(object sender, MouseEventArgs e)
+		{
+			comboBox_Students_Groups.Items.Clear();
+			Choice_Groups_In_Students();
+		}
+
+		private void comboBox_Students_Groups_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string choiced_group = comboBox_Students_Groups.Text;
+
+			int i = tabControl.SelectedIndex;
+			if (comboBox_Students_Groups.SelectedIndex != 0)
+			{
+				comboBox_Students_Groups.Items.Clear();
+				dgvStudents.DataSource = connector.Select(queries[i].Columns, queries[i].Tables, $"{queries[i].Condition} AND group_name = '{choiced_group}'", queries[i].GroupBy);
+				statusStripCountLabel.Text = $"{status_messages[i]} {tables[i].RowCount - 1}";
+			}
+			else
+			{
+				comboBox_Students_Groups.Items.Clear();
+				Choice_Groups_In_Students();
+				tables[i].DataSource = connector.Select(queries[i].Columns, queries[i].Tables, queries[i].Condition, queries[i].GroupBy);
+			}
+			statusStripCountLabel.Text = $"{status_messages[i]} {tables[i].RowCount - 1}";
+		}
+
+		private void checkBox_Empty_Groups_CheckedChanged(object sender, EventArgs e)
+		{
+			int i = tabControl.SelectedIndex;
+			if (checkBox_Empty_Groups.Checked)
+			{
+				dgvGroups.DataSource = connector.Select("group_id, group_name", "Groups", "group_id NOT IN (SELECT [group] FROM Students)");
+				statusStripCountLabel.Text = $"{status_messages[i]} {tables[i].RowCount - 1}";
+			}
+			else
+			{
+				comboBox_Groups_Directions.Items.Clear();
+				Choice_Directions_In_Groups();
+				tables[i].DataSource = connector.Select(queries[i].Columns, queries[i].Tables, queries[i].Condition, queries[i].GroupBy);
+			}
+			statusStripCountLabel.Text = $"{status_messages[i]} {tables[i].RowCount - 1}";
+		}
+
+		private void checkBox_Empty_Directions_CheckedChanged(object sender, EventArgs e)
+		{
+			int i = tabControl.SelectedIndex;
+			if (checkBox_Empty_Directions.Checked)
+			{
+				dgvDirections.DataSource = connector.Select("direction_id, direction_name", "Directions", "direction_id NOT IN (SELECT [direction] FROM Groups)");
+				statusStripCountLabel.Text = $"{status_messages[i]} {tables[i].RowCount - 1}";
+			}
+			else
+			{
+				comboBox_Groups_Directions.Items.Clear();
+				Choice_Directions_In_Groups();
+				tables[i].DataSource = connector.Select(queries[i].Columns, queries[i].Tables, queries[i].Condition, queries[i].GroupBy);
+			}
+			statusStripCountLabel.Text = $"{status_messages[i]} {tables[i].RowCount - 1}";
+		}
+
+		public void Choice_Directions_In_Groups()
+		{
+			comboBox_Groups_Directions.Items.Add("Все");
+			table = connector.Select("direction_name", "Directions");
+
+			foreach (DataRow row in table.Rows)
+			{
+				var cells = row.ItemArray;
+				foreach (object cell in cells)
+				{
+					if (cell != null) comboBox_Groups_Directions.Items.Add(cell);
+				}
+			}
+		}
+
+		
+		public void Choice_Directions_In_Students()
+		{
+			comboBox_Students_Directions.Items.Add("Все");
+			table = connector.Select("direction_name", "Directions");
+
+			foreach (DataRow row in table.Rows)
+			{
+				var cells = row.ItemArray;
+				foreach (object cell in cells)
+				{
+					if (cell != null) comboBox_Students_Directions.Items.Add(cell);
+				}
+			}
+		}
+
+		public void Choice_Groups_In_Students()
+		{
+			comboBox_Students_Groups.Items.Add("Все");
+			table = connector.Select("group_name", "Groups");
+
+			foreach (DataRow row in table.Rows)
+			{
+				var cells = row.ItemArray;
+				foreach (object cell in cells)
+				{
+					if (cell != null) comboBox_Students_Groups.Items.Add(cell);
+				}
+			}
+		}
+
+		
 	}
 }
