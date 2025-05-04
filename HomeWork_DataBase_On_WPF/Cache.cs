900c765ba8a69e8b2f6e63c50e6810e95f59bcec
DmitryK777 <kislyakovdmv@gmail.com>
2025-05-04 16:28:51 +0300

    Home Work WPF in progress

---
+++ HomeWork_DataBase_On_WPF/Cache.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Timers;
+
+using System.Data.SqlClient;
+
+namespace HomeWork_DataBase_On_WPF
+{
+	public class Cache
+	{
+		readonly string CONNECTION_STRING = "";
+		SqlConnection connection = null;
+		DataSet set = null; // Набор таблиц
+
+
+
+		public DataSet Set { get => set; }
+
+		public Cache(string connection_string)
+		{
+			//CONNECTION_STRING = ConfigurationManager.ConnectionStrings["VPD_311_Import"].ConnectionString;
+			CONNECTION_STRING = connection_string;
+			connection = new SqlConnection(CONNECTION_STRING);
+
+			// 1) Создаём DataSet
+			set = new DataSet();
+
+			Console.WriteLine(CONNECTION_STRING);
+
+		}
+
+		public void AddTable(string table, string columns)
+		{
+			// 2.1) Добавляем поля таблицу в DataSet
+			set.Tables.Add(table);
+
+			// 2.2) Добавляем поля (столбики) в таблицу
+			string[] a_columns = columns.Split(',');
+			for (int i = 0; i < a_columns.Length; i++)
+			{
+				set.Tables[table].Columns.Add(a_columns[i]);
+			}
+
+			// 2.3) Определяем какое поле будет первичным ключом
+			set.Tables[table].PrimaryKey =
+				new DataColumn[] { set.Tables[table].Columns[0] };
+
+			string cmd = $"SELECT {columns} FROM {table}";
+			SqlDataAdapter adapter = new SqlDataAdapter(cmd, connection);
+			adapter.Fill(set.Tables[table]);
+			Print(table);
+		}
+
+
+
+		public void AddRelation(string relation_name, string child, string parent)
+		{
+			set.Relations.Add
+				(
+					relation_name,
+					set.Tables[parent.Split(',')[0]].Columns[parent.Split(',')[1]],
+					set.Tables[child.Split(',')[0]].Columns[child.Split(',')[1]]
+				);
+		}
+
+		public void Print(string table)
+		{
+
+
+			Console.WriteLine("\n====================================================================\n");
+			for (int i = 0; i < set.Tables[table].Columns.Count; i++)
+				Console.Write(set.Tables[table].Columns[i].Caption + "\t");
+			Console.WriteLine("\n--------------------------------------------------------------------\n");
+
+			int number_of_parents = set.Tables[table].ParentRelations.Count;
+			for (int i = 0; i < number_of_parents; i++)
+			{
+				Console.WriteLine(set.Tables[table].ParentRelations[i].ToString());
+			}
+			Console.WriteLine(set.Tables[table].ParentRelations.Contains("GroupsDirections"));
+			for (int i = 0; i < set.Tables[table].Rows.Count; i++)
+			{
+				//Console.Write(GroupsRelatedData.Tables[table].Rows[i]+ ":\t");
+				for (int j = 0; j < set.Tables[table].Columns.Count; j++)
+				{
+
+					if
+						(
+							HasParents(table) &&
+							set.Tables[table].ParentRelations[0].ChildColumns.Contains(set.Tables[table].Columns[j])
+						)
+					{
+						string parent_relation_name = !HasParents(table) ? "" :
+						$"{set.Tables[table].TableName}{set.Tables[table].Columns[j].ColumnName}s";
+						Console.WriteLine
+							(
+								//set.Tables[table].ParentRelations[0].ParentColumns[$"{set.Tables[table].Columns[j].ColumnName}_name"]
+								set.Tables[table].Rows[i].GetParentRow(parent_relation_name)[$"{set.Tables[table].Columns[j].ColumnName}_name"]
+							);
+					}
+					else
+						Console.Write(set.Tables[table].Rows[i][j] + "\t\t");
+
+					/*
+					string parent_relation_name = !HasParents(table) ? "" :
+						$"{set.Tables[table].TableName}{set.Tables[table].Columns[j].ColumnName}s";
+
+					if (set.Tables[table].ParentRelations.Contains(parent_relation_name) && )
+						Console.WriteLine
+							(
+								set.
+								Tables[table].
+								Rows[i].
+								GetParentRow(parent_relation_name)[$"{set.Tables[table].Columns[i].ColumnName}_name"]
+							);
+					else 
+					*/
+
+
+
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine("\n====================================================================\n");
+		}
+
+		public bool HasParents(string table)
+		{
+			return set.Tables[table].ParentRelations.Count > 0;
+		}
+
+		public void PrintGroups()
+		{
+			Console.WriteLine("\n====================================================================\n");
+			string table = "Groups";
+			for (int i = 0; i < set.Tables[table].Columns.Count; i++)
+				Console.Write(set.Tables[table].Columns[i].Caption + "\t");
+			Console.Write("\tdirection_name");
+			Console.WriteLine("\n--------------------------------------------------------------------\n");
+			for (int i = 0; i < set.Tables[table].Rows.Count; i++)
+			{
+				for (int j = 0; j < set.Tables[table].Columns.Count; j++)
+				{
+					Console.Write(set.Tables[table].Rows[i][j] + "\t\t");
+
+				}
+				Console.Write(set.Tables[table].Rows[i].GetParentRow("GroupsDirections")["direction_name"]);
+				Console.WriteLine();
+			}
+			Console.WriteLine("\n====================================================================\n");
+		}
+	}
+}
--- HomeWork_DataBase_On_WPF/MainWindow.xaml.cs
+++ HomeWork_DataBase_On_WPF/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +18,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+using System.Data.SqlClient;
+using System.Configuration;
+
+
 namespace HomeWork_DataBase_On_WPF
 {
 	/// <summary>
@@ -20,9 +30,57 @@ namespace HomeWork_DataBase_On_WPF
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		Cache cache;
+		String CONNECTION_STRING = "";
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			AllocConsole();
+
+
+			/*
+			// 1) Создаём DataSet
+
+			cache = new Cache(ConfigurationManager.ConnectionStrings["VPD_311_Import"].ConnectionString);
+			
+
+			//set = new DataSet("GroupsRelatedData");
+
+			cache.AddTable("Directions", "direction_id,direction_name");
+			cache.AddTable("Groups", "group_id,group_name,direction");
+			cache.AddRelation("GroupsDirections", "Groups,direction", "Directions,direction_id");
+			cache.PrintGroups();
+			//cache.Print("Groups");
+			//LoadGroupsRelatedData();
+
+			Console.WriteLine(cache.HasParents("Directions"));
+			Console.WriteLine(cache.HasParents("Groups"));
+
+			cbDirections.DataSource = cache.Set.Tables["Directions"];
+			//cbDirections.DataContext = cache.Set.Tables["Directions"];
+			cbDirections.ValueMember = "direction_id";
+			cbDirections.DisplayMember = "direction_name";
+
+			cbGroups.DataSource = cache.Set.Tables["Groups"];
+			//cbGroups.DataContext = cache.Set.Tables["Groups"];
+			cbGroups.ValueMember = "group_id";
+			cbGroups.DisplayMember = "group_name";
+			*/
+
+		}
+
+		[DllImport("kernel32.dll")]
+		public static extern bool AllocConsole();
+		[DllImport("kernel32.dll")]
+		public static extern bool FreeConsole();
+
+		private void cbDirections_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			object selectedValue = (sender as ComboBox).SelectedValue;
+			string filter = $"direction = {selectedValue.ToString()}";
+			Console.WriteLine(filter);
+			cache.Set.Tables["Groups"].DefaultView.RowFilter = filter;
 		}
 	}
 }
